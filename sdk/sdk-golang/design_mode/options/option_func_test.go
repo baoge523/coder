d657a22df2f0d3a154ad81670d5a948a458a5cf4
yongzzhao <yongzzhao@tencent.com>
2024-09-09 11:39:02 +0800

    添加git相关的学习笔记

---
+++ sdk/sdk-golang/design_mode/options/option_func_test.go
@@ -0,0 +1,66 @@
+package options
+
+import "testing"
+
+// 使用匿名函数的方式实现option
+
+// 定义一个OptionFunc 该类型是一个接受*Options的函数
+type OptionFunc func(*Options)
+
+
+func WithAddrFunc(addr string) OptionFunc {
+	return func(opts *Options) {
+		opts.addr = addr
+	}
+}
+
+func WithHostFunc(host string) OptionFunc {
+	return func(opts *Options) {
+		opts.host = host
+	}
+}
+
+func WithPortFunc(port string) OptionFunc {
+	return func(opts *Options) {
+		opts.port = port
+	}
+}
+
+func WithDbTypeFunc(dbType string) OptionFunc {
+	return func(opts *Options) {
+		opts.dbType = dbType
+	}
+}
+
+func WithCacheFunc(cache bool) OptionFunc {
+	return func(opts *Options) {
+		opts.cache = cache
+	}
+}
+
+func WithTimeoutFunc(timeout int64) OptionFunc {
+	return func(opts *Options) {
+		opts.timeout = timeout
+	}
+}
+
+func NewDBOptionFunc(optionFunc ...OptionFunc) DB{
+	options := &Options{}
+	for _, o := range optionFunc {
+		o(options)
+	}
+	return  NewDB(options.addr,options.host,options.port,options.dbType,options.cache,options.timeout)
+}
+
+func TestOptionFunc(t *testing.T) {
+	db := NewDBOptionFunc(
+		WithAddrFunc("127.0.0.1:3306"),
+		WithHostFunc("127.0.0.1"),
+		WithPortFunc("3306"),
+		WithDbTypeFunc("mysql"),
+		//WithCacheFunc(true),
+		//WithTimeoutFunc(20000),
+	)
+	println(db.string())
+
+}
---
+++ sdk/sdk-golang/design_mode/options/options_test.go
@@ -0,0 +1,145 @@
+package options
+
+import (
+	"fmt"
+	"testing"
+)
+
+// option模式适用于参数比较多的情况下，这样有利于拓展
+
+
+type DB struct {
+	addr string
+	host string
+	port string
+	dbType string
+	cache bool
+	timeout int64
+	// ...
+}
+
+func NewDB(addr string,host string,port string,dbType string,cache bool,timeout int64) DB{
+	db := DB{
+		addr: addr,
+		host: host,
+		port: port,
+		dbType: dbType,
+		cache: cache,
+		timeout: timeout,
+	}
+	return db
+}
+
+func NewDBWithOption(option ...Option) DB{
+	options := &Options{}
+	for _, o := range option {
+		o.apply(options)
+	}
+	return NewDB(options.addr,options.host,options.port,options.dbType,options.cache,options.timeout)
+}
+
+func (d DB) string() string {
+	return fmt.Sprint(d)
+}
+
+
+
+func TestNoOption(t *testing.T) {
+	db := NewDB( "127.0.0.1:3306", "127.0.0.1", "3306", "mysql", true, /*timeout*/1000)
+	println(db.string())
+}
+
+// 记录所有的配置项
+type Options struct {
+	addr string
+	host string
+	port string
+	dbType string
+	cache bool
+	timeout int64
+}
+
+// Option 接口，只有一个函数，这样会导致每一个类型都需要实现这个接口
+// 可以将 Option 定义成接受*Options的函数
+type Option interface {
+	apply(*Options)
+}
+
+type addrType string
+
+func (a addrType) apply(ops *Options) {
+	ops.addr = string(a)
+}
+
+func WithAddr(addr string) Option {
+	return addrType(addr)
+}
+
+type cacheType bool
+
+func (a cacheType) apply(opts *Options) {
+	opts.cache = bool(a)
+}
+
+func WithCache(cache bool) Option{
+	return cacheType(cache)
+}
+
+type hostType string
+
+func (a hostType) apply(opts *Options) {
+	opts.host = string(a)
+}
+
+func WithHost(host string) Option {
+	return hostType(host)
+}
+
+type portType string
+
+func (a portType) apply(opts *Options) {
+	opts.port = string(a)
+}
+
+func WithPort(port string) Option {
+	return portType(port)
+}
+
+type dbType string
+
+func (a dbType) apply(opts *Options) {
+	opts.dbType = string(a)
+}
+
+func WithDbType(dt string) Option {
+	return dbType(dt)
+}
+
+// 每一个参数都需要单独定义，是不是太不友好了，是不是可以弄成匿名函数
+type timeoutType int64
+
+func (a timeoutType) apply(opts *Options) {
+	opts.timeout = int64(a)
+}
+
+func WithTimeOut(timeout int64) Option {
+	return timeoutType(timeout)
+}
+
+
+
+
+func TestUseOption(t *testing.T) {
+	db := NewDBWithOption(
+		WithAddr("127.0.0.1:3306"),
+		WithHost("127.0.0.1"),
+		WithPort("3306"),
+		WithDbType("mysql"),
+		WithCache(true),
+		WithTimeOut(1000),
+	)
+	println(db.string())
+}
+
+
+
